834a63ef093628da0be440473179432e98ed7c51
Maria Negri <mariamnegri@gmail.com>
2021-07-28 13:17:08 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import React , {useState, useEffect} from "react";
 import axios from 'axios';
 import "./App.css";
 
-// const NewpicFc = () =>{
-//   const[newpic,setNewpic] = useState({})
-//   useEffect(() =>{
-//      axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-//      .then(response => {
-//         console.log(response.data)
-//         setNewpic(response.data)
-//            }
-//         )
-//      .catch(error => console.log(error))
-//   },[])
 
 function App() {
 
+const[newpic,setNewpic] = useState({})
+
+useEffect(() =>{
+     axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+     .then(response => {
+        console.log(response.data)
+        setNewpic(response.data)
+           }
+        )
+     .catch(error => console.log(error))
+  },[])
+
   return (
     <div className="App">
-      <p>
-       {/* <NewpicFc /> */}
-      </p>
+      <h1>'This is the picture of day ${newpic.date}'</h1>
+       <h2>{newpic.title}</h2>
+    
+       <div>
+         <img src = {newpic.url}/>
+      </div>
     </div>
   );
 }

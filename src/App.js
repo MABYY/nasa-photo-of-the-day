a76a50a2610ad92c8dd38ff5fe8f7a2c23076cab
Maria Negri <mariamnegri@gmail.com>
2021-07-28 12:04:16 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React , {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
 
+// const NewpicFc = () =>{
+//   const[newpic,setNewpic] = useState({})
+//   useEffect(() =>{
+//      axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+//      .then(response => {
+//         console.log(response.data)
+//         setNewpic(response.data)
+//            }
+//         )
+//      .catch(error => console.log(error))
+//   },[])
+
 function App() {
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+       {/* <NewpicFc /> */}
       </p>
     </div>
   );
